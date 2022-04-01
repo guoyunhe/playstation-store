fb82d483e9dd435d3c5090e3c54a803391f6d484
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-04-01 18:22:27 +0800

    feat: games

---
+++ src/layouts/BasicLayout/menuConfig.ts
@@ -0,0 +1,18 @@
+const headerMenuConfig = [];
+
+const asideMenuConfig = [
+  {
+    name: 'Games',
+    path: '/',
+  },
+  {
+    name: 'XBox Games',
+    path: '/xbox',
+  },
+  {
+    name: 'Nintendo Games',
+    path: '/nintendo',
+  },
+];
+
+export { headerMenuConfig, asideMenuConfig };
