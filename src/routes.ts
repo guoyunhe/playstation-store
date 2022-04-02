fd6745924bdd620983158e659b2a1acc7ac77be5
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-04-02 10:43:57 +0800

    add routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,9 @@ import { IRouterConfig, lazy } from 'ice';
 import BasicLayout from '@/layouts/BasicLayout';
 
 const Games = lazy(() => import('@/pages/games'));
+const NintendoGames = lazy(() => import('@/modules/nintendo-store/src/pages/games'));
+const XboxGames = lazy(() => import('@/modules/xbox-store/src/pages/games'));
+
 const routerConfig: IRouterConfig[] = [
   {
     path: '/',
@@ -11,6 +14,16 @@ const routerConfig: IRouterConfig[] = [
         path: '/',
         exact: true,
         component: Games,
+      },
+      {
+        path: '/nintendo',
+        exact: true,
+        component: NintendoGames,
+      },
+      {
+        path: '/xbox',
+        exact: true,
+        component: XboxGames,
       },
     ],
   },
